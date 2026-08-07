74b7947733ba8e86ed727a0b07ee3dc38745e760
sobhanbaqmirani86 <sobhan.baqmirani86@gmail.com>
2026-08-08 00:53:47 +0330

    Implement Telegram bot with start command

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
 
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ربات پیکسیم فعاله ✅")
+
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
